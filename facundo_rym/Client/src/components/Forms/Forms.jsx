9b6360623d88066570a64418bb0d489f07baa2c6
Facundovila03 <facundovila03@gmail.com>
2023-03-25 17:54:23 -0300

    mas estilos para login

--- facundo_rym/Client/src/components/Forms/Forms.jsx
+++ facundo_rym/Client/src/components/Forms/Forms.jsx
@@ -29,15 +29,22 @@ export default function Form (props){
     return(
         <div className={styles.Contenedor}>
         <form className={styles.form} onSubmit={handleSubmit}>
-            <div className={styles.username} >
-                <label htmlFor="username">Username:</label>
-                <input type='text'  name="username"  value={userData.username} onChange={handleInputChange} placeholder={errors.username}></input>
-                <p> {errors.username}</p>
-            </div>
+            {/* <div className={styles.PasswordContainer} >
+                <label htmlFor="username" >Username:</label>
+                <input type='text'  name="username"  value={userData.username} onChange={handleInputChange} className={styles.PasswordInput}></input>
+                <div className={styles.PasswordWarning} style={errors.username?{opacity:1}:{opacity:0}}> &#9888;</div>
+                <p className={styles.warningMessagePass}>{errors.username}</p>
+            </div> */}
             <div className={styles.PasswordContainer}>
-                <label htmlFor="password" >Password:</label>
-                <input type='text' name="password" onChange={handleInputChange} value={userData.password} className={styles.PasswordInput}></input>
-                <div className={styles.PasswordWarning}>x</div>
+                <label htmlFor="username"  >Username:</label>
+                <input type='text' name="username" onChange={handleInputChange} value={userData.username} className={styles.UsernameInput}></input>
+                <div className={styles.UsernameWarning} style={errors.username?{display:'block'}:{display:'none'}}> &#9888;</div>
+                <p className={styles.warningMessageUser}>{errors.username}</p>
+            {/* </div>
+            <div className={styles.PasswordContainer}> */}
+                <label htmlFor="password"  >Password:</label>
+                <input type='password' name="password" onChange={handleInputChange} value={userData.password} className={styles.PasswordInput}></input>
+                <div className={styles.PasswordWarning} style={errors.password?{display:'block'}:{display:'none'}}> &#9888;</div>
                 <p className={styles.warningMessagePass}>{errors.password}</p>
             </div>
             <button className={styles.login} >login</button>
